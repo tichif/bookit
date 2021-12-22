040fed01578923cebbeb1f12cf2e8554af1b883d
Dalzon Charles-Hébert <charlby5@gmail.com>
2021-12-21 22:38:04 -0500

    Get all rooms in the backend

--- controllers/rooms.js
+++ controllers/rooms.js
@@ -3,11 +3,22 @@ import Room from '../models/Room';
 // @path    /api/rooms
 // @desc    Get all rooms
 // @access  Public
-const getAllRooms = (req, res) => {
-  res.status(200).json({
-    success: true,
-    message: 'Get all rooms',
-  });
+const getAllRooms = async (req, res) => {
+  try {
+    const rooms = await Room.find({});
+
+    res.status(200).json({
+      success: true,
+      count: rooms.length,
+      rooms,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      success: false,
+      error: error.message,
+    });
+  }
 };
 
 // @path    /api/rooms
