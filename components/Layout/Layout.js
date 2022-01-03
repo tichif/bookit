e10c260c3926a810c13eedd4e3590f0b17c7cd90
Dalzon Charles-Hébert <charlby5@gmail.com>
2022-01-03 11:15:38 -0500

    Create the Layout component

---
+++ components/Layout/Layout.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Head from 'next/head';
+
+import Header from './Header';
+import Footer from './Footer';
+
+const Layout = ({ children, title = 'Book Best Hotels for Yor Holydays' }) => {
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+        <meta charSet='utf-8' />
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      </Head>
+      <Header />
+      {children}
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
