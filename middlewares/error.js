20a2b2dc71b96465639c538082ae96650c3b7e03
Dalzon Charles-Hébert <charlby5@gmail.com>
2022-01-03 09:19:24 -0500

    Handle mongoose errors

--- middlewares/error.js
+++ middlewares/error.js
@@ -7,6 +7,18 @@ const errorResponse = (err, req, res, next) => {
 
   error.message = err.message;
 
+  // Bad Mongoose Object ID
+  if (err.name === 'CastError') {
+    const message = `Resource not found. Invalid: ${err.path}`;
+    error = new ErrorHandler(message, 400);
+  }
+
+  // Handling mongoose validation error
+  if (err.name === 'ValidationError') {
+    const message = Object.values(err.errors).map((value) => value.message);
+    error = new ErrorHandler(message, 400);
+  }
+
   res.status(err.statusCode).json({
     success: false,
     error,
