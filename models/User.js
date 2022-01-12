ab76faeafc867996f20616e83ecee405ba902ffa
Dalzon Charles-Hébert <charlby5@gmail.com>
2022-01-12 13:17:00 -0500

    Handle the login logic in the backend

--- models/User.js
+++ models/User.js
@@ -53,4 +53,9 @@ UserSchema.pre('save', async function (next) {
   next();
 });
 
+// Compare user password
+UserSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
 export default mongoose.models.User || mongoose.model('User', UserSchema);
---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,50 @@
+import NextAuth from 'next-auth';
+import Providers from 'next-auth/providers';
+
+import User from '../../../models/User';
+import connectDB from '../../../config/db';
+
+export default NextAuth({
+  session: {
+    jwt: true,
+    maxAge: 60 * 60 * 24, // 1d
+  },
+  providers: [
+    Providers.Credentials({
+      async authorize(credentials) {
+        connectDB();
+
+        const { email, password } = credentials;
+
+        // check if email and password are entered
+        if (!email || !password) {
+          throw new Error('Please enter email or password.');
+        }
+
+        const user = await User.findOne({ email }).select('+password');
+
+        if (!user) {
+          throw new Error('Email or password incorrect !!!');
+        }
+
+        const isPasswordMatched = await user.comparePassword(password);
+        if (!isPasswordMatched) {
+          throw new Error('Email or password incorrect !!!');
+        }
+
+        return Promise.resolve(user);
+      },
+    }),
+  ],
+  callbacks: {
+    // check if the user is existed and assign the user to the token
+    jwt: async (token, user) => {
+      user && (token.user = user);
+      return Promise.resolve(token);
+    },
+    session: async (session, token) => {
+      session.user = token.user;
+      return Promise.resolve(session);
+    },
+  },
+});
