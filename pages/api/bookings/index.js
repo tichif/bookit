9cfccafb96000b187d242d069c7d1ca0008f9b8e
Dalzon Charles-Hébert <charlby5@gmail.com>
2022-01-26 13:29:47 -0500

    Make some changes

--- pages/api/bookings/index.js
+++ pages/api/bookings/index.js
@@ -2,12 +2,13 @@ import nc from 'next-connect';
 
 import { createBooking } from '../../../controllers/booking';
 import connectDB from '../../../config/db';
+import { isAuthenticatedUser } from '../../../middlewares/auth';
 import errorResponse from '../../../middlewares/error';
 
 connectDB();
 
 const handler = nc({ onError: errorResponse });
 
-handler.post(createBooking);
+handler.use(isAuthenticatedUser).post(createBooking);
 
 export default handler;
